0d715f3a5fbc00a7b8733ebd4f027aa24a285b4b
susalt <1356467790@qq.com>
2023-07-07 09:36:54 +0800

    增加了新内容

--- chapter6/example6.py
+++ chapter6/example6.py
@@ -22,46 +22,53 @@ if __name__ == '__main__':
 
     '''实例2'''
     # plot1
-    x=np.array([0,6])
-    y=np.array([0,100])
+    # x=np.array([0,6])
+    # y=np.array([0,100])
+    #
+    # plt.subplot(2,2,1)
+    # plt.plot(x,y)
+    # plt.title("plot 1")
+    #
+    # # plot2
+    # x=np.array([1,2,3,4])
+    # y=np.array([1,4,9,16])
+    #
+    # plt.subplot(2,2,2)
+    # plt.plot(x,y)
+    # plt.title("plot 2")
+    #
+    # # plot2
+    # x=np.array([1,2,3,4])
+    # y=np.array([3,5,7,9])
+    #
+    # plt.subplot(2,2,3)
+    # plt.plot(x,y)
+    # plt.title("plot3")
+    #
+    # # plot3
+    # x=np.array([1,2,3,4])
+    # y=np.array([4,5,6,7])
+    #
+    # plt.subplot(2,2,3)
+    # plt.plot(x,y)
+    # plt.title("plot 3")
+    #
+    # # plot4
+    # x=np.array([1,2,3,4])
+    # y=np.array([4,5,6,7])
+    #
+    # plt.subplot(2,2,4)
+    # plt.plot(x,y)
+    # plt.title("plot 4")
+    #
+    # plt.suptitle("test2")
+    # plt.show()
 
-    plt.subplot(2,2,1)
-    plt.plot(x,y)
-    plt.title("plot 1")
+    '''subplots()'''
+    x=np.linspace(0,2*np.pi,400)
+    y=np.sin(x**2)
 
-    # plot2
-    x=np.array([1,2,3,4])
-    y=np.array([1,4,9,16])
-
-    plt.subplot(2,2,2)
-    plt.plot(x,y)
-    plt.title("plot 2")
-
-    # plot2
-    x=np.array([1,2,3,4])
-    y=np.array([3,5,7,9])
-
-    plt.subplot(2,2,3)
-    plt.plot(x,y)
-    plt.title("plot3")
-
-    # plot3
-    x=np.array([1,2,3,4])
-    y=np.array([4,5,6,7])
-
-    plt.subplot(2,2,3)
-    plt.plot(x,y)
-    plt.title("plot 3")
-
-    # plot4
-    x=np.array([1,2,3,4])
-    y=np.array([4,5,6,7])
-
-    plt.subplot(2,2,4)
-    plt.plot(x,y)
-    plt.title("plot 4")
-
-    plt.suptitle("test2")
+    fig,ax=plt.subplots()
+    ax.plot(x,y)
+    ax.set_title('simple plot')
     plt.show()
-
-    ''''''
